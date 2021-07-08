d74dd14b0a04d030c2a27d3dee1e43eab071b312
Nadia Kotegova <nadinekoteg@gmail.com>
2021-07-08 23:51:50 +0300

    fix error 500

--- utils/config.js
+++ utils/config.js
@@ -2,13 +2,11 @@ require('dotenv').config();
 
 const {
   PORT = 3000,
-  // NODE_ENV,
+  NODE_ENV,
   MONGO_DB_URL, JWT_SECRET,
 } = process.env;
-// const MONGO_DB = NODE_ENV === 'production' ? MONGO_DB_URL : 'mongodb://localhost:27017/bitfilmsdb';
-// const JWT_SECRET_USED = NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key';
-const MONGO_DB = MONGO_DB_URL;
-const JWT_SECRET_USED = JWT_SECRET;
+const MONGO_DB = NODE_ENV === 'production' ? MONGO_DB_URL : 'mongodb://localhost:27017/bitfilmsdb';
+const JWT_SECRET_USED = NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key';
 const MONGO_OPTIONS = {
   useNewUrlParser: true,
   useCreateIndex: true,
