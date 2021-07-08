fa47b7c0bf4dcb93f92415426c76685fcc1663c5
Nadia Kotegova <nadinekoteg@gmail.com>
2021-07-08 23:35:56 +0300

    test env

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 
 const {
-  PORT = 3000, NODE_ENV, MONGO_DB_URL, JWT_SECRET,
+  PORT = 3000,
+  // NODE_ENV,
+  MONGO_DB_URL, JWT_SECRET,
 } = process.env;
-const MONGO_DB = NODE_ENV === 'production' ? MONGO_DB_URL : 'mongodb://localhost:27017/bitfilmsdb';
-const JWT_SECRET_USED = NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key';
+// const MONGO_DB = NODE_ENV === 'production' ? MONGO_DB_URL : 'mongodb://localhost:27017/bitfilmsdb';
+// const JWT_SECRET_USED = NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key';
+const MONGO_DB = MONGO_DB_URL;
+const JWT_SECRET_USED = JWT_SECRET;
 const MONGO_OPTIONS = {
   useNewUrlParser: true,
   useCreateIndex: true,
